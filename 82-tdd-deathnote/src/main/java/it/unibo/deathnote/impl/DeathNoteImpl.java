4ba9a351c067c9bb81f590de26dc67cb04a50246
Massimiliano Ria <massimiliano.ria@studio.unibo.it>
2025-11-23 22:04:43 +0100

    Completed Exercise 82

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -1,49 +1,112 @@
 package it.unibo.deathnote.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import it.unibo.deathnote.api.DeathNote;
 
 public class DeathNoteImpl implements DeathNote {
 
+    private final Map<String, Data> notes;
+    private String lastName;
+
+    private final static class Data {
+        String deathCause;
+        String deathDetails;
+        long nameTimestamp;
+        long causeTimestamp;
+    }
+
+    public DeathNoteImpl() {
+        this.notes = new HashMap<>();
+    }
+
     @Override
     public String getRule(int ruleNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRule'");
+        if (ruleNumber < 1 || ruleNumber > RULES.size()) {
+            throw new IllegalArgumentException("Rule number out of range");
+        }
+        return RULES.get(ruleNumber - 1);
     }
 
     @Override
     public void writeName(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+        Objects.requireNonNull(name);
+        this.lastName = name;
+        this.notes.put(name, new Data());
+        this.notes.get(name).nameTimestamp = System.currentTimeMillis();
+        this.notes.get(name).causeTimestamp = System.currentTimeMillis();
+        this.notes.get(name).deathCause = "heart attack";
+        this.notes.get(name).deathDetails = "";
     }
 
     @Override
     public boolean writeDeathCause(String cause) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+        if (cause == null) {
+            throw new IllegalStateException("Death Cause cannot be null");
+        }
+        if (this.lastName == null) {
+            throw new IllegalStateException("DeathNote cannot be empty");
+        }
+        final Data lastNameData = this.notes.get(lastName);
+        final long lastNameTimestamp = lastNameData.nameTimestamp;
+        final long currentTime = System.currentTimeMillis();
+        if (currentTime - lastNameTimestamp <= 40) {
+            lastNameData.deathCause = cause;
+            lastNameData.causeTimestamp = currentTime;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean writeDetails(String details) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+        if (details == null) {
+            throw new IllegalStateException("Details cannot be null");
+        }
+        if (this.lastName == null) {
+            throw new IllegalStateException("DeathNote cannot be empty");
+        }
+        final Data lastNameData = this.notes.get(lastName);
+        final long currentTime = System.currentTimeMillis();
+        final long lastNameCauseTimestamp = lastNameData.causeTimestamp;
+        if (currentTime - lastNameCauseTimestamp <= 6040) {
+            lastNameData.deathDetails = details;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public String getDeathCause(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        final Data nameData = this.notes.get(name);
+        if (nameData == null) {
+            throw new IllegalArgumentException("Name is not written in DeathNote");
+        }
+        return nameData.deathCause;
     }
 
     @Override
     public String getDeathDetails(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        final Data nameData = this.notes.get(name);
+        if (nameData == null) {
+            throw new IllegalArgumentException("Name is not written in DeathNote");
+        }
+        return nameData.deathDetails;
     }
 
     @Override
     public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+        return this.notes.containsKey(name);
     }
     
 }
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,13 +1,16 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.unibo.deathnote.api.DeathNote;
 import it.unibo.deathnote.impl.DeathNoteImpl;
 
 /**
@@ -43,4 +46,58 @@ class TestDeathNote {
         }
     }
 
+    @Test
+    void checkNoEmptyOrNullRule() {
+        for (final String rule : DeathNote.RULES) {
+            assertNotNull(rule, "Rule should not be null");
+            assertFalse(rule.isBlank(), "Rule should not be blank or empty");
+        }
+    }
+
+    @Test
+    void checkNameCorrectlyWritten() {
+        assertFalse(deathnote.isNameWritten("Lucas"));
+        deathnote.writeName("Lucas");
+        assertTrue(deathnote.isNameWritten("Lucas"));
+        assertFalse(deathnote.isNameWritten("Luca"));
+        assertFalse(deathnote.isNameWritten(""));
+    }
+
+    @Test
+    void checkDeathCauseIsCorrectlyManaged() throws InterruptedException {
+        try {
+            deathnote.writeDeathCause("eating a nail");
+            fail("Expected IllegalStateException when writing death cause before writing a name");
+        } catch (IllegalStateException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+        }
+        deathnote.writeName("Dustin");
+        assertEquals("heart attack", deathnote.getDeathCause("Dustin"));
+        deathnote.writeName("Will");
+        assertTrue(deathnote.writeDeathCause("karting accident"));
+        assertEquals("karting accident", deathnote.getDeathCause("Will"));
+        Thread.sleep(100);
+        assertFalse(deathnote.writeDeathCause("car accident"));
+        assertNotEquals("car accident", deathnote.getDeathCause("Will"));
+    }
+
+    @Test
+    void checkDeathDetailsAreCorrectlyManaged() throws InterruptedException {
+        try {
+            deathnote.writeDetails("a lot of pain");
+            fail("Expected IllegalStateException when writing death details before writing a name");
+        } catch (IllegalStateException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());           
+        }
+        deathnote.writeName("Mike");
+        assertEquals("", deathnote.getDeathDetails("Mike"));
+        assertTrue(deathnote.writeDetails("ran for too long"));
+        assertEquals("ran for too long", deathnote.getDeathDetails("Mike"));
+        deathnote.writeName("Steve");
+        Thread.sleep(6100);
+        assertFalse(deathnote.writeDetails("riding an horse"));
+        assertNotEquals("riding an horse", deathnote.getDeathDetails("Steve"));
+    }
 }
