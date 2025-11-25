ed1b3d6b2b2f70dbc37a98ec0f94ccd6ce815351
Massimiliano Ria <massimiliano.ria@studio.unibo.it>
2025-11-25 16:28:28 +0100

    Completed Exercise 83. Corrected

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,9 +1,11 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.InvocationTargetException;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
@@ -23,9 +25,20 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args)
+        throws 
+        ClassNotFoundException, 
+        NoSuchMethodException, 
+        InstantiationException, 
+        IllegalAccessException, 
+        InvocationTargetException {
+            
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
+
+        for (int i = 0; i < 3; i++) {
+           app.addView( (DrawNumberView) Class.forName("it.unibo.mvc.view.DrawNumberStandardOutputView").getConstructor().newInstance());
+           app.addView( (DrawNumberView) Class.forName("it.unibo.mvc.view.DrawNumberSwingView").getConstructor().newInstance());
+        }
     }
 }
